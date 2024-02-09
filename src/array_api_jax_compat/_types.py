1f467b9091a48cd7bb649f992e602bf42d58b2e1
nstarman <nstarman@users.noreply.github.com>
2024-02-09 10:40:52 -0500

    misc updates
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/array_api_jax_compat/_types.py
+++ src/array_api_jax_compat/_types.py
@@ -7,14 +7,14 @@ __all__: list[str] = []
 
 from typing import Any, Protocol, TypeVar, runtime_checkable
 
-import numpy as np
+import jax.numpy as jnp
 
 
 @runtime_checkable
 class DType(Protocol):
     """The dtype of an array."""
 
-    dtype: np.dtype[Any]
+    dtype: jnp.dtype[Any]
 
 
 @runtime_checkable  # TODO: need actual implementation
@@ -29,6 +29,8 @@ _T_co = TypeVar("_T_co", covariant=True)
 class NestedSequence(Protocol[_T_co]):
     """A nested sequence."""
 
-    def __getitem__(self, key: int, /) -> "_T_co | NestedSequence[_T_co]": ...
+    def __getitem__(self, key: int, /) -> "_T_co | NestedSequence[_T_co]":
+        ...
 
-    def __len__(self, /) -> int: ...
+    def __len__(self, /) -> int:
+        ...
