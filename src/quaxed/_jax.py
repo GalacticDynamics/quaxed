8d2a5f315fc37f4bc54207b691d0cda3eb9795ef
nstarman <nstarman@users.noreply.github.com>
2024-03-14 20:04:23 -0400

    feat: quaxify grad
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/quaxed/_jax.py
+++ src/quaxed/_jax.py
@@ -2,9 +2,40 @@
 
 __all__ = [
     "device_put",
+    "grad",
 ]
+
+from collections.abc import Callable, Hashable, Sequence
+from typing import Any, TypeAlias
 
 import jax
 from quax import quaxify
 
+AxisName: TypeAlias = Hashable
+
+
+# =============================================================================
+
 device_put = quaxify(jax.device_put)
+
+
+def grad(  # noqa: PLR0913
+    fun: Callable[..., Any],
+    argnums: int | Sequence[int] = 0,
+    *,
+    has_aux: bool = False,
+    holomorphic: bool = False,
+    allow_int: bool = False,
+    reduce_axes: Sequence[AxisName] = (),
+) -> Callable[..., Any]:
+    """Quaxed version of :func:`jax.grad`."""
+    return quaxify(
+        jax.grad(
+            fun,
+            argnums,
+            has_aux=has_aux,
+            holomorphic=holomorphic,
+            allow_int=allow_int,
+            reduce_axes=reduce_axes,
+        )
+    )
