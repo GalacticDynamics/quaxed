2778d91af2b1eb5a814f2c785619919661ecde62
nstarman <nstarman@users.noreply.github.com>
2024-05-07 16:41:47 -0400

    fix: only iinfo
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/quaxed/array_api/_data_type_functions.py
+++ src/quaxed/array_api/_data_type_functions.py
@@ -2,15 +2,15 @@ __all__ = ["astype", "can_cast", "finfo", "iinfo", "isdtype", "result_type"]
 
 
 from jax.experimental import array_api
+from jax.experimental.array_api._data_type_functions import FInfo
 from jaxtyping import ArrayLike
 
 from quaxed._types import DType
 from quaxed._utils import JAX_VERSION, quaxify
 
 if JAX_VERSION < (0, 4, 27):
-    from jax.experimental.array_api._data_type_functions import FInfo, IInfo
+    from jax.experimental.array_api._data_type_functions import IInfo
 else:
-    from jax.experimental.array_api import finfo as FInfo  # noqa: N812
     from jax.experimental.array_api import iinfo as IInfo  # noqa: N812
 
 
