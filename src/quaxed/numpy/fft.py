8b28b0c9e894c36370c49c25ffcd296ed288a5d5
nstarman <nstarman@users.noreply.github.com>
2024-09-17 00:03:01 -0400

    feat: fft
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/quaxed/numpy/fft.py
+++ src/quaxed/numpy/fft.py
@@ -1,3 +1,38 @@
-"""Quaxed :mod:`jax.numpy.fft`."""
+"""FFT functions."""
 
-__all__: list[str] = []
+__all__ = [
+    "fft",
+    "ifft",
+    "fftn",
+    "ifftn",
+    "rfft",
+    "irfft",
+    "rfftn",
+    "irfftn",
+    "hfft",
+    "ihfft",
+    "fftfreq",
+    "rfftfreq",
+    "fftshift",
+    "ifftshift",
+]
+
+
+from jax.numpy import fft as _jax_fft
+
+from quaxed._utils import quaxify
+
+fft = quaxify(_jax_fft.fft)
+ifft = quaxify(_jax_fft.ifft)
+fftn = quaxify(_jax_fft.fftn)
+ifftn = quaxify(_jax_fft.ifftn)
+rfft = quaxify(_jax_fft.rfft)
+irfft = quaxify(_jax_fft.irfft)
+rfftn = quaxify(_jax_fft.rfftn)
+irfftn = quaxify(_jax_fft.irfftn)
+hfft = quaxify(_jax_fft.hfft)
+ihfft = quaxify(_jax_fft.ihfft)
+fftfreq = quaxify(_jax_fft.fftfreq)
+rfftfreq = quaxify(_jax_fft.rfftfreq)
+fftshift = quaxify(_jax_fft.fftshift)
+ifftshift = quaxify(_jax_fft.ifftshift)
