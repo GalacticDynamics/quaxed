bc2a4f48c699dc7e393d74588cb6990b7df916fa
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-09-17 00:10:03 -0400

    feat: fft (#82)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/quaxed/array_api/fft.py
+++ src/quaxed/array_api/fft.py
@@ -17,152 +17,22 @@ __all__ = [
     "ifftshift",
 ]
 
-from collections.abc import Sequence
-from typing import Literal
 
-from jax import Device
 from jax.experimental.array_api import fft as _jax_fft
-from jaxtyping import ArrayLike
-from quax import Value
 
 from quaxed._utils import quaxify
 
-
-@quaxify
-def fft(
-    x: ArrayLike,
-    /,
-    *,
-    n: int | None = None,
-    axis: int = -1,
-    norm: Literal["backward", "ortho", "forward"] = "backward",
-) -> Value:
-    return _jax_fft.fft(x, n=n, axis=axis, norm=norm)
-
-
-@quaxify
-def ifft(
-    x: ArrayLike,
-    /,
-    *,
-    n: int | None = None,
-    axis: int = -1,
-    norm: Literal["backward", "ortho", "forward"] = "backward",
-) -> Value:
-    return _jax_fft.ifft(x, n=n, axis=axis, norm=norm)
-
-
-@quaxify
-def fftn(
-    x: ArrayLike,
-    /,
-    *,
-    s: Sequence[int] | None = None,
-    axes: Sequence[int] | None = None,
-    norm: Literal["backward", "ortho", "forward"] = "backward",
-) -> Value:
-    return _jax_fft.fftn(x, s=s, axes=axes, norm=norm)
-
-
-@quaxify
-def ifftn(
-    x: ArrayLike,
-    /,
-    *,
-    s: Sequence[int] | None = None,
-    axes: Sequence[int] | None = None,
-    norm: Literal["backward", "ortho", "forward"] = "backward",
-) -> Value:
-    return _jax_fft.ifftn(x, s=s, axes=axes, norm=norm)
-
-
-@quaxify
-def rfft(
-    x: ArrayLike,
-    /,
-    *,
-    n: int | None = None,
-    axis: int = -1,
-    norm: Literal["backward", "ortho", "forward"] = "backward",
-) -> Value:
-    return _jax_fft.rfft(x, n=n, axis=axis, norm=norm)
-
-
-@quaxify
-def irfft(
-    x: ArrayLike,
-    /,
-    *,
-    n: int | None = None,
-    axis: int = -1,
-    norm: Literal["backward", "ortho", "forward"] = "backward",
-) -> Value:
-    return _jax_fft.irfft(x, n=n, axis=axis, norm=norm)
-
-
-@quaxify
-def rfftn(
-    x: ArrayLike,
-    /,
-    *,
-    s: Sequence[int] | None = None,
-    axes: Sequence[int] | None = None,
-    norm: Literal["backward", "ortho", "forward"] = "backward",
-) -> Value:
-    return _jax_fft.rfftn(x, s=s, axes=axes, norm=norm)
-
-
-@quaxify
-def irfftn(
-    x: ArrayLike,
-    /,
-    *,
-    s: Sequence[int] | None = None,
-    axes: Sequence[int] | None = None,
-    norm: Literal["backward", "ortho", "forward"] = "backward",
-) -> Value:
-    return _jax_fft.irfftn(x, s=s, axes=axes, norm=norm)
-
-
-@quaxify
-def hfft(
-    x: ArrayLike,
-    /,
-    *,
-    n: int | None = None,
-    axis: int = -1,
-    norm: Literal["backward", "ortho", "forward"] = "backward",
-) -> Value:
-    return _jax_fft.hfft(x, n=n, axis=axis, norm=norm)
-
-
-@quaxify
-def ihfft(
-    x: ArrayLike,
-    /,
-    *,
-    n: int | None = None,
-    axis: int = -1,
-    norm: Literal["backward", "ortho", "forward"] = "backward",
-) -> Value:
-    return _jax_fft.ihfft(x, n=n, axis=axis, norm=norm)
-
-
-@quaxify
-def fftfreq(n: int, /, *, d: float = 1.0, device: Device | None = None) -> Value:
-    return _jax_fft.fftfreq(n, d=d, device=device)
-
-
-@quaxify
-def rfftfreq(n: int, /, *, d: float = 1.0, device: Device | None = None) -> Value:
-    return _jax_fft.rfftfreq(n, d=d, device=device)
-
-
-@quaxify
-def fftshift(x: ArrayLike, /, *, axes: int | Sequence[int] | None = None) -> Value:
-    return _jax_fft.fftshift(x, axes=axes)
-
-
-@quaxify
-def ifftshift(x: ArrayLike, /, *, axes: int | Sequence[int] | None = None) -> Value:
-    return _jax_fft.ifftshift(x, axes=axes)
+fft = quaxify(_jax_fft.fft)
+ifft = quaxify(_jax_fft.ifft)
+fftn = quaxify(_jax_fft.fftn)
+ifftn = quaxify(_jax_fft.ifftn)
+rfft = quaxify(_jax_fft.rfft)
+irfft = quaxify(_jax_fft.irfft)
+rfftn = quaxify(_jax_fft.rfftn)
+irfftn = quaxify(_jax_fft.irfftn)
+hfft = quaxify(_jax_fft.hfft)
+ihfft = quaxify(_jax_fft.ihfft)
+fftfreq = quaxify(_jax_fft.fftfreq)
+rfftfreq = quaxify(_jax_fft.rfftfreq)
+fftshift = quaxify(_jax_fft.fftshift)
+ifftshift = quaxify(_jax_fft.ifftshift)
