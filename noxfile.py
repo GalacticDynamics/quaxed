cefd578c7aa47df95ffc537c7120400687091373
nstarman <nstarman@users.noreply.github.com>
2025-11-23 15:31:04 -0500

    docs: update docs
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- noxfile.py
+++ noxfile.py
@@ -1,114 +1,81 @@
 """Nox setup."""
 
-import argparse
 import shutil
 from pathlib import Path
 
 import nox
+from nox_uv import session
+
+nox.needs_version = ">=2024.3.2"
+nox.options.sessions = [
+    # Linting
+    "lint",
+    "pylint",
+    "precommit",
+    # Testing
+    "tests",
+    # Documentation
+    "docs",
+    "build_api_docs",
+]
+nox.options.default_venv_backend = "uv"
 
 DIR = Path(__file__).parent.resolve()
 
-nox.options.sessions = ["lint", "pylint", "tests"]
+# =============================================================================
+# Linting
 
 
-@nox.session
-def lint(session: nox.Session) -> None:
+@session(uv_groups=["lint"], reuse_venv=True)
+def lint(s: nox.Session, /) -> None:
     """Run the linter."""
-    session.install("pre-commit")
-    session.run(
-        "pre-commit",
-        "run",
-        "--all-files",
-        "--show-diff-on-failure",
-        *session.posargs,
-    )
+    precommit(s)  # reuse pre-commit session
+    pylint(s)  # reuse pylint session
 
 
-@nox.session
-def pylint(session: nox.Session) -> None:
+@session(venv_backend="uv", reuse_venv=True)
+def precommit(s: nox.Session, /) -> None:
+    """Run pre-commit."""
+    s.run("pre-commit", "run", "--all-files", *s.posargs)
+
+
+@session(uv_groups=["lint"], reuse_venv=True)
+def pylint(s: nox.Session, /) -> None:
     """Run PyLint."""
-    # This needs to be installed into the package environment, and is slower
-    # than a pre-commit check
-    session.install(".", "pylint")
-    session.run("pylint", "quaxed", *session.posargs)
+    s.install(".", "pylint")
+    s.run("pylint", "quaxed", *s.posargs)
 
 
-@nox.session
-def tests(session: nox.Session) -> None:
+# =============================================================================
+# Testing
+
+
+@session(uv_groups=["test"], reuse_venv=True)
+def tests(s: nox.Session, /) -> None:
     """Run the unit and regular tests."""
-    session.install(".[test]")
-    session.run("pytest", *session.posargs)
+    s.run("pytest", *s.posargs)
 
 
-@nox.session(reuse_venv=True)
-def docs(session: nox.Session) -> None:
+# =============================================================================
+# Documentation
+
+
+@session(uv_groups=["docs"], reuse_venv=True)
+def docs(s: nox.Session, /) -> None:
     """Build the docs. Pass "--serve" to serve. Pass "-b linkcheck" to check links."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--serve", action="store_true", help="Serve after building")
-    parser.add_argument(
-        "-b",
-        dest="builder",
-        default="html",
-        help="Build target (default: html)",
-    )
-    args, posargs = parser.parse_known_args(session.posargs)
-
-    if args.builder != "html" and args.serve:
-        session.error("Must not specify non-HTML builder with --serve")
-
-    extra_installs = ["sphinx-autobuild"] if args.serve else []
-
-    session.install("-e.[docs]", *extra_installs)
-    session.chdir("docs")
-
-    if args.builder == "linkcheck":
-        session.run(
-            "sphinx-build",
-            "-b",
-            "linkcheck",
-            ".",
-            "_build/linkcheck",
-            *posargs,
-        )
-        return
-
-    shared_args = (
-        "-n",  # nitpicky mode
-        "-T",  # full tracebacks
-        f"-b={args.builder}",
-        ".",
-        f"_build/{args.builder}",
-        *posargs,
-    )
-
-    if args.serve:
-        session.run("sphinx-autobuild", *shared_args)
-    else:
-        session.run("sphinx-build", "--keep-going", *shared_args)
+    s.run("mkdocs", "build", *s.posargs)
 
 
-@nox.session
-def build_api_docs(session: nox.Session) -> None:
-    """Build (regenerate) API docs."""
-    session.install("sphinx")
-    session.chdir("docs")
-    session.run(
-        "sphinx-apidoc",
-        "-o",
-        "api/",
-        "--module-first",
-        "--no-toc",
-        "--force",
-        "../src/quaxed",
-    )
+# =============================================================================
+# Build
 
 
-@nox.session
-def build(session: nox.Session) -> None:
+@session(uv_groups=["build"])
+def build(s: nox.Session, /) -> None:
     """Build an SDist and wheel."""
     build_path = DIR.joinpath("build")
     if build_path.exists():
         shutil.rmtree(build_path)
 
-    session.install("build")
-    session.run("python", "-m", "build")
+    s.install("build")
+    s.run("build")
